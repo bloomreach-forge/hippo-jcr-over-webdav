593867dc62f277b033339db087b4624316268e49
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-04-18 14:39:09 -0400

    Adding demo and generated site docs

---
+++ demo/site/src/main/java/org/onehippo/forge/repository/server/remoting/davex/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.repository.server.remoting.davex.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hipporepojcrdavdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
