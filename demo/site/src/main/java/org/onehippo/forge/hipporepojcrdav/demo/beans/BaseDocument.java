030b6ff3170136569501f8d12846e51612fdcffc
Woonsan Ko <woonsan.ko@bloomreach.com>
2018-10-03 12:55:12 -0400

    Updating demo project based on v12

---
+++ demo/site/src/main/java/org/onehippo/forge/hipporepojcrdav/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.hipporepojcrdav.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hipporepojcrdavdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
